f14bb4a198a3ace3e9e19a96d9a0e99796b1adc2
AniekanSamson <samsonubonganiekan@gmail.com>
2023-02-23 08:50:31 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,24 +8,23 @@
  *
  * Return: a pointer to the resulting string @dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int i;
+        int len1 = 0;
+        int len2 = 0;
+        int i;
 
-	while (src[len1] != '\0')
-		len1++;
+        while (src[len1] != '\0')
+                len1++;
 
-	while (dest[len2] != '\0')
-		len2++;
+        while (dest[len2] != '\0')
+                len2++;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[len2] =src[i];
-		len2++;
-	}
+        for (i = 0; i < n; i++)
+        {
+                dest[len2] =src[i];
+                len2++;
+        }
 
-	return (dest);
+        return (dest);
 }
