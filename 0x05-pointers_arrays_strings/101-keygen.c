1033f1fde2d3845860fe2ac856b39bfe69d85224
AniekanSamson <samsonubonganiekan@gmail.com>
2023-03-03 14:55:17 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,34 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer.
- * @s: input string.
- * Return: integer.
+ * main - entry point
+ *
+ * Return: generated password
  */
-int _atoi(char *s)
+
+int main(void)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	char c;
+	int x;
 
-	while (*(s + count) != '\0')
+	srand(time(0));
+	while (x <= 2645)
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (*(s + count) == '-')
-			pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		count++;
+		c = rand() % 128;
+		x += c;
+		putchar(c);
 	}
+	putchar(2772 - x);
 
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	return (0);
 }
