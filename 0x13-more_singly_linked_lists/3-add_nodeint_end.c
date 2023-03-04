48eaa145a1ff8a04091596fee5c9855adc4805b2
AniekanSamson <samsonubonganiekan@gmail.com>
2023-03-04 14:15:36 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,34 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "lists.h"
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * add_nodeint_end - Entry Point
+ * @head: head
+ * @n: value
+ * Return: 0
  */
-typedef struct listint_s
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *new;
+	listint_t *temp;
 
-int _putchar(char c);
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-#endif /* LISTS_H */
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
